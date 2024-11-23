ec3c1c2df90a6018fafdae6a92a8831a4f617a0c
gottsch <mark.gottschling@gmail.com>
2024-11-23 15:02:58 -0500

    adding all spells

---
+++ src/main/java/mod/gottsch/fabric/magic_treasures/core/mixin/ItemStackMixin.java
@@ -0,0 +1,30 @@
+/*
+ * This file is part of  Magic Treasures.
+ * Copyright (c) 2024 Mark Gottschling (gottsch)
+ *
+ * Magic Treasures is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Magic Treasures is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Magic Treasures.  If not, see <http://www.gnu.org/licenses/lgpl>.
+ */
+package mod.gottsch.fabric.magic_treasures.core.mixin;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+
+/**
+ * Created by Mark Gottschling on 11/23/2024
+ */
+@Mixin(ItemStack.class)
+public class ItemStackMixin {
+
+}
